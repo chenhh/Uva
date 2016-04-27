919e2cf9023563a54a439dcc46435858d1b7dae8
Hung-Hsin Chen <chen1116@gmail.com>
2016-04-27 10:59:26 +0800

    Update uva_623.py

--- uva_623.py
+++ uva_623.py
@@ -1,1 +1,6 @@
-# https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=564
+# -*- coding: utf-8 -*-
+"""
+Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
+License: GPL v2
+https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=564
+"""
