f8dfeb6b508a13fcb71ab2be87797ab331f0173b
chenhh <chenhh@x220>
2016-04-28 01:44:55 +0800

    11192 working

---
+++ uva_11192.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
+License: GPL v2
+https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=23&page=show_problem&problem=2133
+http://luckycat.kshs.kh.edu.tw/homework/q11192.htm
+"""
+
+def main():
+
+    while True:
+        data = input()
+        print data.split()
+
+if __name__ == '__main__':
+    main()
