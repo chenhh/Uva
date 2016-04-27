6ca76613f2c8c68c4d955fbf7041b4119fd2f3d7
Hung-Hsin Chen <chen1116@gmail.com>
2016-04-27 11:17:09 +0800

    Update uva_623.py

--- uva_623.py
+++ uva_623.py
@@ -4,3 +4,12 @@ Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
 License: GPL v2
 https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=564
 """
+import math
+while True:
+	try:
+		v = input()
+		value = int(v)
+		print ("{}!".format(value))
+		print ("{}".format(math.factorial(value)))
+	except (EOFError):
+		break
