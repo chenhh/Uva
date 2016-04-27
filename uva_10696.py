13c445bdd034ddad66956092b75145bbe283f8a4
chenhh <chenhh@x220>
2016-04-28 01:38:05 +0800

    10696 AC

--- uva_10696.py
+++ uva_10696.py
@@ -4,17 +4,24 @@ Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
 License: GPL v2
 https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=18&page=show_problem&problem=1637
 http://luckycat.kshs.kh.edu.tw/homework/q10696.htm
+
+n >= 101, f91(n) = n-10
+n <= 100 f91(n) = f91(f91(n+11))
+
+f91(90) = f91(f91(101))
+= f91(91) = f91(f91(102))
+= f91(92) = f91(f91(103))
+= f91(93) = f91(f91(104))
+= ......
+= f91(99) = f91(f91(105))
+= f91(100) = f91(f91(111))
+= f91(101) = 91
+
+ f91(N) = f91(f91(N+11))
+= ... = f91(f91(f91(...f91(a)))) // 90 <= a <= 100
+= f91(f91(f91(...f91(91))))
+= 91
 """
-MAX=1000001
-
-cache = [0] * MAX
-
-def f91(n):
-    if n<=MAX and cache[n] != 0:
-        return cache[n]
-
-
-
 def main():
     while True:
         val = input()
@@ -22,7 +29,8 @@ def main():
         if n == 0:
             break
 
-
+        sol = 91 if n<=100 else  n-10
+        print ("f91({}) = {}".format(n, sol))
 
 if __name__ == '__main__':
     main()
