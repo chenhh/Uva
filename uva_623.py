e70e5671d5d941f908e654fe0febe19095f10cfe
Hung-Hsin Chen <chen1116@gmail.com>
2016-04-28 08:25:13 +0800

    Update uva_623.py

--- uva_623.py
+++ uva_623.py
@@ -3,6 +3,7 @@
 Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
 License: GPL v2
 https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=564
+status:AC
 """
 import math
 
