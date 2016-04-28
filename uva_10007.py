ebb0cad0f147dd5a85873d67ebdfdf7315d683bf
Hung-Hsin Chen <chen1116@gmail.com>
2016-04-28 08:26:04 +0800

    Update uva_10007.py

--- uva_10007.py
+++ uva_10007.py
@@ -3,6 +3,7 @@
 Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
 License: GPL v2
 https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=948
+status:AC
 
 If given no. of Nodes are N Then
     Different No. of BST=Catalan(N)
