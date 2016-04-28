80f19207cecce2ad7e2e6cff07f10cd36df13c41
Hung-Hsin Chen <chen1116@gmail.com>
2016-04-28 15:38:08 +0800

    494 working

---
+++ uva_494.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
+License: GPL v2
+status: 
+https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=6&page=show_problem&problem=435
+http://luckycat.kshs.kh.edu.tw/homework/q494.htm
+"""
+
+def main():
+
+    while True:
+        try:
+            strs = input()
+            words =strs.split()
+        except (EOFError):
+            break
+if __name__ == '__main__':
+    main()
