b14ab64c1c247931b4188edf36b49a1c3b7f45a9
Hung-Hsin Chen <chen1116@gmail.com>
2016-04-28 09:00:01 +0800

    11192 AC

--- uva_11192.py
+++ uva_11192.py
@@ -2,6 +2,7 @@
 """
 Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
 License: GPL v2
+status: AC
 https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=23&page=show_problem&problem=2133
 http://luckycat.kshs.kh.edu.tw/homework/q11192.htm
 """
@@ -10,7 +11,12 @@ def main():
 
     while True:
         data = input()
-        print data.split()
+        if data == "0":
+            break
+        v, strs = data.split()
+        n_grp = int(v)
+        grp_len = len(strs) // n_grp
+        print ("".join([strs[idx*grp_len:(idx+1)*grp_len][::-1] for idx in range(n_grp)]))
 
 if __name__ == '__main__':
     main()
