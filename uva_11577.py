1d539cf798b0c8f366889fdd40df32667a285617
Hung-Hsin Chen <chen1116@gmail.com>
2016-04-28 10:15:14 +0800

    11577 AC

---
+++ uva_11577.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
+License: GPL v2
+status: AC
+https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=27&page=show_problem&problem=2624
+http://luckycat.kshs.kh.edu.tw/homework/q11577.htm
+"""
+
+def main():
+    v = input()
+    n_data = int(v)
+    for cnt in range(n_data):
+        inputs = input()
+        lowers = inputs.lower()
+        alpha_cnt = [0] * 26
+        for alpha in lowers:
+            if 'a' <= alpha <='z':
+                alpha_cnt[ord(alpha) - ord('a')] += 1
+        max_value = max(alpha_cnt)
+        max_alphas = [chr(idx+ord('a')) for idx, cnt in enumerate(alpha_cnt) if max_value == cnt]
+        print ("".join(max_alphas))
+            
+if __name__ == '__main__':
+    main()
