651eb805f16b8f39c1593afa91338ff77a53abfe
chenhh <chenhh@x220>
2016-05-06 20:13:16 +0800

    11713 AC

--- utility/graph.py
+++ utility/graph.py
@@ -3,32 +3,28 @@
 Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
 License: GPL v2
 """
-
-def dfs_iterative(graph, start_node):
-    """
-    the graph contains node from 0 ~ (n-1)
-    assume the graph will be strongly connected
-
-    Parameters:
-    -----------------------
-    graph: list of list, graph[idx] contains the list of edges
-    start_node，integer, the dfs start node
-
-    Return:
-    -----------------------
-    sequences: list, the sequence of DFS traversal
-    """
+def dfs_bicolor(graph, start_node):
+    # print (graph)
     stack = [start_node,]
-    walked = [False] * len(graph)
-    sequences = []
+    n_node = len(graph)
+    colors= [-1] * n_node
+    colors[start_node] = 0
+    walked = [False] * n_node
+    bicolor = True
 
     while len(stack) > 0:
         node = stack.pop()
-        if not walked[node]:
-            print (node)
-            sequences.append(node)
-            for child in graph[node]:
-                if not walked[child]:
-                    stack.append(child)
+        if not bicolor:
+            break
 
-    return sequences
+        if not walked[node]:
+            walked[node] = True
+            for child in graph[node]:
+                if colors[child] == -1:
+                    colors[child] = 1 - colors[node]
+                elif colors[child] != 1 - colors[node]:
+                    bicolor = False
+                    break
+                stack.append(child)
+    return bicolor
+es
---
+++ uva_11713.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
+License: GPL v2
+status: AC
+difficulty: 1
+
+https://uva.onlinejudge.org/external/117/11713.pdf
+hist
+heat
+NO
+
+goths
+gates
+NO
+
+jumps
+james
+NO
+
+"""
+
+def main():
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    n = int(input())
+    for _ in range(n):
+        real = input()
+        abstract = input()
+
+        if len(real) != len(abstract):
+            print ("No")
+        elif real == abstract:
+            print ("Yes")
+        else:
+            mutant = True
+            for r, a in zip(real, abstract):
+                if r not in vowels and r != a:
+                    mutant = False
+                elif r in vowels and a not in vowels:
+                    mutant = False
+
+                if not mutant:
+                    break
+            if mutant:
+                print ("Yes")
+            else:
+                print ("No")
+
+if __name__ == '__main__':
+    main()
+
+
