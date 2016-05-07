d3f964d99ede45a62455e4a3acf2aebb72ba01d1
chenhh <chenhh@x220>
2016-05-07 22:47:25 +0800

    10226 C++ AC, buy python3 TLE

---
+++ uva_10226.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+using namespace std;
+
+int main()
+{
+    string dummy;
+    int number_of_cases;
+    cin >> number_of_cases;
+    getline(cin, dummy);
+    getline(cin, dummy);
+    for (int idx = 0; idx < number_of_cases; idx++)
+    {
+        map<string, int> tree_count;
+        int count = 0;
+        while (true) {
+            string tree;
+            getline(cin, tree);
+            if (tree.length() == 0) {
+                break;
+            }
+            map<string, int>::iterator probe = tree_count.find(tree);
+            if (probe == tree_count.end())
+                tree_count.insert(pair<string, int>(tree, 1));
+            else
+                tree_count[tree]++;
+            count++;
+        }
+        double factor = 100.0 / count;
+        cout << setprecision(4) << fixed;
+        map<string, int>::iterator iter;
+        for ( iter= tree_count.begin(); iter != tree_count.end(); iter++){
+            cout << iter->first << " " <<iter->second * factor << endl;
+        }
+
+        if (idx != number_of_cases - 1)
+            cout << endl;
+    }
+    return 0;
+}
