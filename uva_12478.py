790ca7c198c80053df77f7d87905f05093f143a4
chenhh <chenhh@x220>
2016-05-08 04:11:16 +0800

    12478 AC

---
+++ uva_12478.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
+License: GPL v2
+status: AC
+difficulty: 1
+https://uva.onlinejudge.org/external/124/12478.pdf
+"""
+print ("KABIR")
