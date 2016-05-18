bb427b5246bcb4081f92269e6eb6a5767e09de82
chenhh <chenhh@x220>
2016-05-18 11:41:31 +0800

    11909 AC

--- uva_11909.py
+++ uva_11909.py
@@ -6,14 +6,28 @@ License: GPL v2
 status: AC
 difficulty: 1
 https://uva.onlinejudge.org/external/119/11909.pdf
+
+computes the volume of soya milk remaining in the packet
 """
+import math
 
 def main():
     while 1:
         try:
-            vals = list(map(int, input().split()))
+            # length, width, height, angle <= 90
+            l, w, h, angle = list(map(int, input().split()))
+            rad = angle/180 * math.pi
+            h2 = l * math.tan(rad)
+            if h2 <= h:
+                # above the diagonal
+                ans = l * w * (h - h2/2)
+            else:
+                # below the diagonal
+                l2 = h/math.tan(rad)
+                ans = l2 * w * h  / 2
+            print ("{:.3f} mL".format(ans))
 
-        except:
+        except (EOFError):
             break
 
 if __name__ == '__main__':
