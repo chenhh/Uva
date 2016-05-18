45b2ea6af884a394417aa8dc609a094860531cfd
chenhh <chenhh@x220>
2016-05-18 13:34:43 +0800

    11909 reformat code

--- uva_11909.py
+++ uva_11909.py
@@ -11,24 +11,26 @@ computes the volume of soya milk remaining in the packet
 """
 import math
 
+
 def main():
     while 1:
         try:
             # length, width, height, angle <= 90
             l, w, h, angle = list(map(int, input().split()))
-            rad = angle/180 * math.pi
+            rad = angle / 180 * math.pi
             h2 = l * math.tan(rad)
             if h2 <= h:
                 # above the diagonal
-                ans = l * w * (h - h2/2)
+                ans = l * w * (h - h2 / 2)
             else:
                 # below the diagonal
-                l2 = h/math.tan(rad)
-                ans = l2 * w * h  / 2
-            print ("{:.3f} mL".format(ans))
+                l2 = h / math.tan(rad)
+                ans = l2 * w * h / 2
+            print("{:.3f} mL".format(ans))
 
         except (EOFError):
             break
+
 
 if __name__ == '__main__':
     main()
