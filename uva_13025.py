2fbfd46370dbc364fc49e95a200e99a181aa372c
chenhh <chenhh@x220>
2016-05-20 20:01:32 +0800

    13025 AC

---
+++ uva_13025.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
+License: GPL v2
+
+status: AC
+difficulty: 1
+https://uva.onlinejudge.org/external/130/13025.pdf
+"""
+
+print ("May 29, 2013 Wednesday")
