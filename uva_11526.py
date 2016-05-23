227720a90ed3f832f560eeb31e4457ace0e207a8
chenhh <chenhh@x220>
2016-05-23 19:58:01 +0800

    11526 python AC

--- uva_11526.py
+++ uva_11526.py
@@ -3,7 +3,7 @@
 Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
 License: GPL v2
 
-status: TLE
+status: AC
 difficulty: 1
 
 https://uva.onlinejudge.org/external/115/11526.pdf
@@ -20,9 +20,9 @@ def orig_H(n):
 
 def H(n):
     """ complexity : O(lg n) """
-    root = int(math.sqrt(n)) +1
+    root = int(math.sqrt(n)) + 1
     res = sum(n//idx for idx in range(1, root))
-    root -=1
+    root -= 1
     res = res*2 - root*root
     return res
 
@@ -31,8 +31,10 @@ def main():
     T = int(input())
     for _ in range(T):
         n = int(input())
-        print(H(n))
-        print (sum(n//idx for idx in range(1, n+1)))
+        if n<=0:
+            print (0)
+        else:
+            print(H(n))
 
 
 if __name__ == '__main__':
