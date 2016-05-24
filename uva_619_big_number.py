d0c66abe930f62326919f9a26cbd19b13369f465
Hung-Hsin Chen <chen1116@gmail.com>
2016-05-24 19:51:33 +0800

    619 AC

--- uva_619_big_number.py
+++ uva_619_big_number.py
@@ -2,10 +2,8 @@
 """
 Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
 License: GPL v2
-
 status: AC
 difficulty: 1
-
 https://uva.onlinejudge.org/external/6/619.pdf
 http://luckycat.kshs.kh.edu.tw/homework/q619.htm
 """
@@ -16,30 +14,33 @@ def main():
     data = sys.stdin.readlines()
 
     for line in data:
-        if line == '*':
-            break
         num = line.strip()
-
+        if num == '*':
+            break
+        
         if  num[0].isalpha():
             # word to value
             digits = list(num)
             value = ord(digits[0]) - ord('a') + 1
             for d in digits[1:]:
                 value = 26 * value +  (ord(d) - ord('a') + 1)
-            print ("{:<22}{:<23,}".format(num, value))
+            print ("{:<22}{:<,}".format(num, value))
 
         elif num[0].isnumeric():
             # value to word
             value = int(num)
             digits = []
-            while value >= 26:
+            while value > 0:
                 a, b = divmod(value, 26)
-                if a and not b:
+                if not b:
                     digits.append('z')
-
-                digits.append(chr(b+ord('a')))
-                value = a
-            print("{:<22}{:<23,}".format("".join(reversed(digits)), int(num)))
+                    value = a -1
+                    if a== 1:
+                        break
+                else:
+                    digits.append(chr(b+ord('a')-1))
+                    value = a
+            print("{:<22}{:<,}".format("".join(reversed(digits)), int(num)))
 
 
 
