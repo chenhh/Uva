8fa5d724288e58fb4e461a8fe7d6039bdd21d11a
chenhh <chen1116@gmail.com>
2016-05-26 11:51:11 +0800

    10919 AC

--- uva_10919.py
+++ uva_10919.py
@@ -7,13 +7,41 @@ status: AC
 difficulty: 1
 
 https://uva.onlinejudge.org/external/109/10919.pdf
+http://luckycat.kshs.kh.edu.tw/homework/q10919.htm
 """
 
 
 def main():
-    n_course, n_category = list(map(int, input().split()))
-    for _ in range(n_course):
-        input()
+    while 1:
+        record = input().split()
+        if record[0] == '0':
+            break
+        n_chosen, n_category = list(map(int, record))
+
+        # One or more lines follow containing k 4-digit integers follow
+        chosen = []
+        for _ in range(n_chosen):
+            chosen.extend(input().split())
+            if len(chosen) >= n_chosen:
+                break
+
+        satisfied = True
+        for _ in range(n_category):
+            data = input().split()
+            n_course = int(data[0])
+            n_required = int(data[1])
+            courses = frozenset(data[2:])
+
+            cnt = 0
+            for c in chosen:
+                if c in courses:
+                    cnt += 1
+                if cnt >= n_required:
+                    break
+            if cnt < n_required:
+                satisfied = False
+
+        print("yes" if satisfied else "no")
 
 
 if __name__ == '__main__':
