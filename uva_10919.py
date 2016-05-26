51387fb7799308428a72dd74f7be57d89da8b599
chenhh <chenhh@x220>
2016-05-26 11:12:52 +0800

    19019 working

---
+++ uva_10919.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
+License: GPL v2
+
+status: AC
+difficulty: 1
+
+https://uva.onlinejudge.org/external/109/10919.pdf
+"""
+
+
+def main():
+    n_course, n_category = list(map(int, input().split()))
+    for _ in range(n_course):
+        input()
+
+
+if __name__ == '__main__':
+    main()
