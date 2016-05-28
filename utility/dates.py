cb5c2ae05e01f334d94c8a0d56ad069e17f4db81
chenhh <chenhh@x220>
2016-05-28 15:08:36 +0800

    update date utility

--- utility/dates.py
+++ utility/dates.py
@@ -46,3 +46,24 @@ def date_shift(year, month, day, shift):
         tgt_month += 1
 
     return (tgt_year, tgt_month + 1, days)
+
+
+def time_zone(hour, min, am_pm, src_zone, tgt_zone):
+    """ uva 10371 """
+    zones = {
+        'UTC': 0, 'GMT': 0, 'BST': 1, 'IST': 1, 'WET': 0, 'WEST': 1,
+        'CET': 1, 'CEST': 2, 'EET': 2, 'EEST': 3, 'MSK': 3, 'MSD': 4,
+        'AST': -4, 'ADT': -3, 'NST': -3.5, 'NDT': -2.5, 'EST': -5, 'EDT': -4,
+        'CST': -6, 'CDT': -5, 'MST': -7, 'MDT': -6, 'PST': -8, 'PDT': -7,
+        'HST': -10, 'AKST': -9, 'AKDT': -8, 'AEST': 10, 'AEDT': 11,
+        'ACST': 9.5, 'ACDT': 10.5, 'AWST': 8
+    }
+
+    base_min = (hour % 12) * 60 + min
+    base_min += 720 if am_pm == "p.m." else 0
+    tgt_base_min = int(base_min + (zones[tgt_zone] - zones[src_zone]) * 60)
+    tgt_base_min %= 1440
+    tgt_hour, tgt_min = divmod(tgt_base_min, 60)
+    tgt_am_pm = 'a.m.' if tgt_base_min < 720 else 'p.m.'
+    tgt_hour -= 12 if tgt_hour > 12 else 0
+    return (tgt_hour if tgt_hour else 12, tgt_min, tgt_am_pm)
