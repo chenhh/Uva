568a05c85ff421f9cd88f0483d0bd5eff0b66a68
chenhh <chenhh@x220>
2016-06-19 13:33:28 +0800

    upate queens with a fixed position module

--- utility/graph.py
+++ utility/graph.py
@@ -194,3 +194,56 @@ def queens_iterative(n_queen=8):
                 # the position does not place a new queen
                 cols[cdx], lefts[l_idx], rights[r_idx] = False, False, False
     return ans
+
+
+def queens_with_a_fixed_position(x, y, n_queen=8):
+    """
+    uva 750
+    backtracking, searching by row first then by column.
+    the size of the board is n_row * n_col (rdx from 0 to n_row -1 (n_col - 1)
+    if there is a queen placed in board[rdx][cdx], then
+        the rdx row, the cdx column, the diagonal (rdx+cdx)%(2*(n_col)-1), and
+        the diagonal ((rdx-cdx)+(2*(n_col)-1))%((2*(n_col)-1) can't place
+        another queen.
+
+    x, y is the location of a given queen.
+    """
+    # square chessboard
+    n_row = n_col = n_queen
+    d_size = (2 * n_col - 1)
+    ans = []
+
+    # initial condition
+    i_cols = [False] * n_col
+    i_cols[y] = True
+    i_lefts, i_rights = [False] * d_size, [False] * d_size
+    i_lefts[(x + y) % d_size], i_rights[(x - y) % d_size] = True, True
+
+    # (rdx, cols, left_diagonal, right_diagonal, positions)
+    stack = [(0, i_cols, i_lefts, i_rights, [(x, y)])]
+
+    while stack:
+        rdx, cols, lefts, rights, positions = stack.pop()
+        # print (rdx, cols, positions)
+        if rdx >= n_row:
+            # because the xth row will not be checked, there are only
+            # n_row -1 rows need to be checked
+            if len(positions) == n_queen:
+                ans.append(positions)
+            continue
+
+        if rdx == x:
+            stack.append((rdx + 1, cols[:], lefts[:], rights[:],
+                          positions[:]))
+            continue
+
+        for cdx in range(n_col):
+            l_idx, r_idx = (rdx + cdx) % d_size, (rdx - cdx) % d_size
+            if not any((cols[cdx], lefts[l_idx], rights[r_idx])):
+                # the position can place a new queen
+                cols[cdx], lefts[l_idx], rights[r_idx] = True, True, True
+                stack.append((rdx + 1, cols[:], lefts[:], rights[:],
+                              positions + [(rdx, cdx)]))
+                # the position does not place a new queen
+                cols[cdx], lefts[l_idx], rights[r_idx] = False, False, False
+    return ans
