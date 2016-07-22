ef58fb64be3884ca333c90c4e481528c0bfeb98e
chenhh <chenhh@x220>
2016-07-23 00:14:58 +0800

    116 AC

--- uva_116_DP.py
+++ uva_116_DP.py
@@ -15,29 +15,57 @@ The start can be any row (not necessary from the first row).
 """
 
 import sys
-from pprint import pprint
 
-directions = ((0, 1), (1, 1), (-1, 1))
-
+INT_MAX = 2 << 31
 
 def solve(weights, n_row, n_col):
     """
     the value in the maze may be negative integer.
     """
-    global directions
     cost = [weights[idx][:] for idx in range(n_row)]
+    path = [[INT_MAX] * n_col for idx in range(n_row)]
 
-    # backtrack
-    path = [-1] * n_col
-    for idx in range(n_row):
-        for jdx in range(1, n_col):
-            cost[idx][jdx] = min(
-                cost[idx][jdx - 1],  # from left
-                cost[(idx - 1) % n_row][jdx - 1],  # from upper left
-                cost[(idx + 1) % n_row][jdx - 1],  # from lower left
-            ) + weights[idx][jdx]
+    # traceback
+    for jdx in reversed(range(n_col - 1)):
+        for idx in range(n_row):
+            w = weights[idx][jdx]
+            left = cost[idx][jdx + 1] + w
+            upper = cost[(idx - 1) % n_row][jdx + 1] + w
+            lower = cost[(idx + 1) % n_row][jdx + 1] + w
 
-    pprint(cost)
+            if left <= upper and left <= lower:
+                cost[idx][jdx] = left
+                path[idx][jdx] = min(path[idx][jdx], idx)
+
+            if upper <= left and upper <= lower:
+                cost[idx][jdx] = upper
+                path[idx][jdx] = min(path[idx][jdx], (idx - 1) % n_row)
+
+            if lower <= left and lower <= upper:
+                cost[idx][jdx] = lower
+                path[idx][jdx] = min(path[idx][jdx], (idx + 1) % n_row)
+
+    # pprint (cost)
+    # pprint(path)
+
+    min_path = [0] * n_col
+    min_weight = cost[0][0]
+    min_path[0] = 0
+    next_rdx = path[0][0]
+    for idx in range(1, n_row):
+        if cost[idx][0] < min_weight:
+            min_weight = cost[idx][0]
+            min_path[0] = idx
+            next_rdx = path[idx][0]
+
+    jdx = 1
+    while jdx < n_col:
+        min_path[jdx] = next_rdx
+        next_rdx = path[next_rdx][jdx]
+        jdx += 1
+
+    print(" ".join(str(v + 1) for v in min_path))
+    print(min_weight)
 
 
 def main():
@@ -55,7 +83,7 @@ def main():
                     weights[rdx][cdx] = val
                     idx += 1
             # process
-            print(solve(weights, n_row, n_col))
+            solve(weights, n_row, n_col)
 
         except (StopIteration):
             break
